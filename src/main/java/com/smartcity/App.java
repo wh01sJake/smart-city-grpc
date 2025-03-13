52d0635829713d0a261f2334d0af263aa83ffd82
Fragile <flk812@gmail.com>
2025-03-13 23:09:15 +0000

    "Initial project setup with Maven and JavaFX"

---
+++ src/main/java/com/smartcity/App.java
@@ -0,0 +1,13 @@
+package com.smartcity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
