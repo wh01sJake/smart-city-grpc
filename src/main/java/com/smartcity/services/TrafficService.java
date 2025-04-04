821594d63e65a8f34133c17ca411abccea895435
Fragile <flk812@gmail.com>
2025-04-04 19:49:25 +0100

    fix: Improve service discovery filtering and error logging

--- src/main/java/com/smartcity/services/TrafficService.java
+++ src/main/java/com/smartcity/services/TrafficService.java
@@ -7,28 +7,34 @@ import com.smartcity.TrafficData;
 import com.smartcity.TrafficGrpc;
 
 import io.grpc.stub.StreamObserver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+
 public class TrafficService extends TrafficGrpc.TrafficImplBase {
+    private static final Logger logger = LogManager.getLogger(TrafficService.class);
+
+
+    //self-register upon initialization
+    public TrafficService() {
+        RegistryService.selfRegister("traffic", "localhost:50051");
+    }
 
     // Simple RPC implementation
     @Override
-    public void setLight(LightCommand request,
-                         StreamObserver<Response> responseObserver) {
-        System.out.println("Changing light at " + request.getIntersection()
-                + " to " + request.getColor());
-        responseObserver.onNext(Response.newBuilder()
-                .setStatus("OK")
-                .build());
+    public void setLight(LightCommand request, StreamObserver<Response> responseObserver) {
+        logger.info("Changing light at {} to {}", request.getIntersection(), request.getColor());
+        responseObserver.onNext(Response.newBuilder().setStatus("OK").build());
         responseObserver.onCompleted();
     }
 
     // Server Streaming implementation
     @Override
-    public void streamTraffic(Empty request,
-                              StreamObserver<TrafficData> responseObserver) {
+    public void streamTraffic(Empty request, StreamObserver<TrafficData> responseObserver) {
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
         executor.scheduleAtFixedRate(() -> {
             TrafficData data = TrafficData.newBuilder()
@@ -37,5 +43,6 @@ public class TrafficService extends TrafficGrpc.TrafficImplBase {
                     .build();
             responseObserver.onNext(data);
         }, 0, 5, TimeUnit.SECONDS);
+        logger.info("Started traffic data streaming");
     }
 }
